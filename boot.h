1b334b75b2d2790bd5c2c2f6ca337bc1428b4f0e
avital676 <58787211+avital676@users.noreply.github.com>
2020-01-09 19:05:41 +0200

    Add files via upload

---
+++ boot.h
@@ -0,0 +1,27 @@
+//
+// Created by avital on 09/01/2020.
+//
+
+#ifndef EX4_BOOT_H
+#define EX4_BOOT_H
+
+#include "Solver.h"
+#include "CacheManager.h"
+#include "server_side.h"
+#include "MyTestClientHandler.h"
+#include "MySerialServer.h"
+int y;
+namespace boot {
+    class Main {
+    public:
+        void main(int port) {
+            Solver<string, string>* sol = new StringReverser();
+            CacheManager<string>* c = new FileCacheManager(5);
+            server_side::Server* ser = new MySerialServer();
+            ClientHandler* cli = new MyTestClientHandler<string>(sol, c);
+            ser->open(port, cli);
+        }
+    };
+}
+
+#endif //EX4_BOOT_H
