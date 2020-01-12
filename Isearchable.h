a04ef595cb41e1b081c7d0e5cd43b54c8a8e017a
avital <avital676@gmail.com>
2020-01-12 19:10:29 +0200

    undefined reference state

--- Isearchable.h
+++ Isearchable.h
@@ -13,9 +13,9 @@ using namespace std;
 template<class T>
 class Isearchable {
 public:
-    virtual state<T>* getInitialState() = 0;
-    virtual bool isGoalStateSate(state<T> s) = 0;
-    virtual vector<state<T>*> getAllPossibleState(state<T> s) = 0;
+    virtual state<T>* getInitialState();
+    virtual bool isGoalStateSate(state<T> s);
+    virtual vector<state<T>*> getAllPossibleState(state<T> s);
 };
 
 #endif //EX4_ISEARCHABLE_H
